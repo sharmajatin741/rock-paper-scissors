27ae88a7583e90484956687ee1d0562fd391ba5a
Jatin Sharma <sharmajatin741@gmail.com>
2023-09-03 10:06:44 +0530

    Added UI to rock paper scissor

--- script.js
+++ script.js
@@ -16,41 +16,106 @@ function getWonMessage(playerSelection, computerSelection) {
 function playGame(playerSelection, computerSelection) {
     if(playerSelection.toLowerCase() === choices[0]) {
         if(computerSelection == choices[0]) {
-            return "Oh! Draw!"
+            return [0, "Oh! Draw!"]
         } else if(computerSelection == choices[1]) {
-            return getLoseMessage(playerSelection, computerSelection);
+            return [-1, getLoseMessage(playerSelection, computerSelection)];
         } else {
-            return getWonMessage(playerSelection, computerSelection);
+            return [1, getWonMessage(playerSelection, computerSelection)];
         }
     } else if(playerSelection.toLowerCase() === choices[1]) {
         if(computerSelection == choices[1]) {
-            return "Oh! Draw!"
+            return [0, "Oh! Draw!"]
         } else if(computerSelection == choices[2]) {
-            return getLoseMessage(playerSelection, computerSelection);
+            return [-1, getLoseMessage(playerSelection, computerSelection)];
         } else {
-            return getWonMessage(playerSelection, computerSelection);
+            return [1, getWonMessage(playerSelection, computerSelection)];
         }
     } else if(playerSelection.toLowerCase() === choices[2]) {
         if(computerSelection == choices[2]) {
-            return "Oh! Draw!"
+            return [0, "Oh! Draw!"]
         } else if(computerSelection == choices[0]) {
-            return getLoseMessage(playerSelection, computerSelection);
+            return [-1, getLoseMessage(playerSelection, computerSelection)];
         } else {
-            return getWonMessage(playerSelection, computerSelection);
+            return [1, getWonMessage(playerSelection, computerSelection)];
         }
     } else {
         return "Wrong Input!";
     }
 }
 
-function game() {
-    for(let i = 1; i <= 5; i++) {
-        let input = prompt("Please enter one of the following possible inputs 'rock', 'paper', 'scissors'");
-        let computerSelection = getComputerChoice();
-        console.log("User Choice - " + input);
-        console.log("Computer Choice - " + computerSelection);
-        console.log(playGame(input, computerSelection));
-        console.log("-------------------------------");
+let computerScore = 0, userScore = 0;
+
+const buttons = document.querySelectorAll("button");
+const output = document.querySelector(".output");
+const scorecard = document.querySelector(".scorecard");
+
+const computerChoice = document.createElement("div");
+const resultShow = document.createElement("div");
+const computerScorecard = document.createElement("div");
+const userScorecard = document.createElement("div");
+const overallWinner = document.createElement("div");
+
+output.appendChild(computerChoice);
+output.appendChild(resultShow);
+scorecard.appendChild(computerScorecard);
+scorecard.appendChild(userScorecard);
+scorecard.appendChild(overallWinner);
+
+let active;
+function optionClick(e) {
+
+    if(active !== undefined) {
+        active.style.background = 'white';
+    }
+    active = this;
+    this.style.background = 'green';
+
+    let computerSelection = getComputerChoice();
+    let result = playGame(this.innerText, computerSelection);
+    
+    updateResult(computerSelection, result);
+    updateScore(result[0]);
+    showOverallResult();
+    updateScorecard();
+
+}
+
+
+function showOverallResult() {
+    if (userScore === 5 || computerScore === 5) {
+        if (userScore === 5 && computerScore === 5) {
+            overallWinner.innerHTML = `Both Player Won`;
+        } else if (userScore === 5) {
+            overallWinner.innerHTML = `You Won`;
+        } else {
+            overallWinner.innerHTML = `Computer Won`;
+        }
+        if(active !== undefined) {
+            active.style.background = 'white';
+        }    
+        buttons.forEach((button) => button.disabled = true);
+
     }
 }
-game()
+
+function updateResult(computerSelection, result) {
+    computerChoice.innerHTML = `Computer Choice - ${computerSelection}`;
+    resultShow.innerHTML = result[1];
+}
+
+function updateScorecard() {
+    computerScorecard.innerHTML = `Computer Score - ${computerScore}`;
+    userScorecard.innerHTML = `User Score - ${userScore}`;
+}
+
+function updateScore(winner) {
+    if(winner === 0) {
+
+    } else if(winner === 1) {
+        userScore++;
+    } else {
+        computerScore++
+    }
+}
+
+buttons.forEach((button) => button.addEventListener('click', optionClick));
